6025b5dacabeff21ca23de95d13b3985b25a99ba
ilnaz <237x237@gmail.com>
2024-10-23 22:21:10 +0300

    fix:📝 delete rollupOptions

--- react-chat/vite.config.js
+++ react-chat/vite.config.js
@@ -5,11 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/2024-2-VK-EDU-Frontend-I-Nazyrov/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx',
-      },
-    },
-  },
 });
